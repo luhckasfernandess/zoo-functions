7a8139b2b073a6825cf26a63329f9442306dba1d
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-16 16:41:33 -0400

    Requirement 2 complete! :heavy_check_mark: getAnimalsOlderThan function implemented

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Procura pelo nome do animal usando o find que retorna o primeiro
+  const animalAge = data.species.find((element) => element.name === 'lions');
+  // Verifica se todos atendem a condição usando o every
+  return animalAge.residents.every((element) => element.age >= age);
 }
-
+// const teste = data.species.find((element) => element.name === 'lions');
+// console.log(teste.residents.every((element) => element.age <= 12));
 module.exports = getAnimalsOlderThan;
