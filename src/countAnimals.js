34809487cfce8adcfc552ca601603e2cc93d6d39
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-22 09:27:04 -0400

    Requirement 5 complete! :heavy_check_mark: Implemented countAnimals function

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
+// data.species
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let countAnimal;
+  if (typeof animal === 'undefined') {
+    countAnimal = {};
+    // Usei o Object.assign para copiar o objeto que eu queria para a variável countAnimal. Abençoado!
+    species.forEach((e) => Object.assign(countAnimal, { [e.name]: e.residents.length }));
+    return countAnimal;
+  }
+  const { specie, sex } = animal;
+  // Object.keys retorna um array do parâmetro que é um objeto
+  if (Object.keys(animal).length === 1) {
+    // find procura pelo nome do animal e o residents.length (array) retorna a quantidade dele
+    countAnimal = species.find((element) => element.name === specie).residents.length;
+    return countAnimal;
+  } if (Object.keys(animal).length === 2) {
+    countAnimal = species.find((e) => e.name === specie).residents;
+    // Filtrei pelo sexo do animal com o filter, que me retorna um array. Com .lenght tenho a qtd
+    return countAnimal.filter((e) => e.sex === sex).length;
+  }
 }
+
+// countAnimals({ specie: 'penguins', sex: 'male' });
 
 module.exports = countAnimals;
