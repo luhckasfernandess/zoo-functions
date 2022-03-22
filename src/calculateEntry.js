bb6c8757bec350bccb822abbf5038acc7a4e8955
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-22 10:59:24 -0400

    Requirement 6 complete! :heavy_check_mark: Now I'm glad I'm moving forward

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const countChild = entrants.filter((e) => e.age < 18).length;
+  const countAdult = entrants.filter((e) => e.age >= 18 && e.age < 50).length;
+  const countSenior = entrants.filter((e) => e.age >= 50).length;
+  const countTotal = { child: countChild, adult: countAdult, senior: countSenior };
+  return countTotal;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let calculate = 0;
+  if (typeof entrants === 'undefined') return calculate;
+  if (Object.keys(entrants).length === 0) return calculate;
+  const count = countEntrants(entrants);
+  // child: 20.99, adult: 49.99, senior: 24.99
+  calculate = count.child * 20.99 + count.adult * 49.99 + count.senior * 24.99;
+  console.log('apos calc', calculate);
+  return calculate;
 }
 
 module.exports = { calculateEntry, countEntrants };
